666a75ab5a2f1b1229fd54592e0bcd5514f3e48b
gxsham <gxsham@gmail.com>
2016-02-22 23:24:05 +0300

    troll

---
+++ client.py
@@ -0,0 +1,23 @@
+import socket, sys
+import time
+s_client = socket.socket()
+
+s_client.connect( ('127.0.0.1', 1045) )
+
+
+while 1:
+     
+    mess = sys.stdin.readline()
+   
+    if mess!="":
+        s_client.send(mess)
+           
+    answer = s_client.recv(25000)
+    if answer!="":        
+        if answer.startswith(b'\xff\xd8'):
+            file = open("newTerminator.jpg", "wb")
+            file.write(answer)
+            file.close()
+            sys.stdout.write("Picture recieved")
+        else:
+            sys.stdout.write(answer)
---
+++ server.py
@@ -0,0 +1,50 @@
+from socket import * 
+import time, sys, io, random , linecache
+from time import gmtime, strftime
+from thread import start_new_thread
+HOST = "127.0.0.1"
+PORT = 1045
+listeningSocket = socket()
+#bind the socket to an interface
+listeningSocket.bind( (HOST, PORT) )
+print "bind = OK"
+#make it listen
+listeningSocket.listen(5) 
+    
+def client_thread(newAllocatedSocket):   
+    while newAllocatedSocket:
+        try:
+            data = newAllocatedSocket.recv(100)
+            sys.stdout.write(data)
+        except:
+            return
+        
+        if data == "hastalavista\n":
+            newAllocatedSocket.close()
+            listeningSocket.close()
+        elif data =="pic\n":
+            file = open('Terminator.jpg','rb') 
+            newAllocatedSocket.send(file.read())
+            file.close()
+        elif data.startswith('reverse'):
+            rever = data[8:]
+            newAllocatedSocket.send(rever[::-1]+'\n') 
+        elif data =="joke\n":
+            newAllocatedSocket.send(linecache.getline('jokes.txt',random.randint(1,12)))
+        elif data =="host\n":
+            newAllocatedSocket.send(HOST+ "\n")
+        elif data =="close\n":
+            newAllocatedSocket.close()
+        elif data =="time\n":
+            newAllocatedSocket.send(strftime("%a, %d %b %Y %H:%M:%S\n", gmtime()))    
+        else:
+            if data.endswith('?\n'):
+                newAllocatedSocket.send('42\n')
+            else:
+                newAllocatedSocket.send("Can you elaborate on that?\n")
+        
+while 1:
+    
+    newAllocatedSocket, address = listeningSocket.accept()   
+    start_new_thread(client_thread,(newAllocatedSocket,))
+    print address, newAllocatedSocket
