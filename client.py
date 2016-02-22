671ff0a0f305c60201aed63581695cf8af23dc70
gxsham <gxsham@gmail.com>
2016-02-23 01:32:19 +0300

    update 2

--- client.py
+++ client.py
@@ -1,17 +1,11 @@
 import socket, sys
 import time
 s_client = socket.socket()
-
 s_client.connect( ('127.0.0.1', 1045) )
-
-
 while 1:
-     
     mess = sys.stdin.readline()
-   
     if mess!="":
         s_client.send(mess)
-           
     answer = s_client.recv(25000)
     if answer!="":        
         if answer.startswith(b'\xff\xd8'):
